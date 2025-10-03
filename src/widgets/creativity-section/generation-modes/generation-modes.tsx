2d01dbe5599086daa67de48675e99573ab146acd
Stanislav <kepamuk@yandex.ru>
2025-10-03 15:07:59 +0300

    feat: add GenerationModes section

--- src/widgets/creativity-section/generation-modes/generation-modes.tsx
+++ src/widgets/creativity-section/generation-modes/generation-modes.tsx
@@ -1,3 +1,50 @@
-export const GenerationModes = () => {
-  return <div>generation-modes</div>;
+import { clsx } from 'clsx';
+
+import RefreshArrow from '@/shared/assets/refreshArrow';
+
+import styles from './generation-modes.module.scss';
+
+interface Props {
+  withoutTitle?: boolean;
+}
+
+export const GenerationModes = ({ withoutTitle = false }: Props) => {
+  return (
+    <div className={styles.container}>
+      {!withoutTitle && (
+        <p className={styles.title}>
+          Далее будет доступно два режима генерации:
+        </p>
+      )}
+      <div className={styles.modes}>
+        <div className={styles.mode}>
+          <p
+            className={clsx(
+              styles.modeTitle,
+              withoutTitle && styles.modeTitleSecondary
+            )}
+          >
+            Generate
+          </p>
+          <p className={styles.modeDescription}>Базовая модель генерации</p>
+        </div>
+        <div className={styles.mode}>
+          <p
+            className={clsx(
+              styles.modeTitle,
+              withoutTitle && styles.modeTitleSecondary
+            )}
+          >
+            <RefreshArrow />- Regenerate
+          </p>
+          <p className={styles.modeDescription}>
+            Генерация на основе выбранного изображения.{' '}
+          </p>
+          <p className={styles.modeDescription}>
+            Улучшает сходство с фотографией, и не только
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
